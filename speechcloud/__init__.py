d876a57d140034bb78b77ef44361b51d513a62a9
Jan Švec <honzas@ntis.zcu.cz>
2026-02-05 11:52:23 +0100

    Fix: Restore missing AiortcSIPHandler export in __init__.py

--- speechcloud/__init__.py
+++ speechcloud/__init__.py
@@ -2,3 +2,7 @@ from .client import SpeechCloud as SpeechCloud
 from .sc_client import SpeechCloudClient as SpeechCloudClient
 from .dialog import SpeechCloudWS as SpeechCloudWS, Dialog as Dialog, ABNF_INLINE as ABNF_INLINE
 from .sip_interface import SIPInterface as SIPInterface
+try:
+    from .sip_aiortc import AiortcSIPHandler as AiortcSIPHandler
+except ImportError:
+    pass
